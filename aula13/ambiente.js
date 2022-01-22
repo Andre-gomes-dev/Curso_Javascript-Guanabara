a72bab26b705a9e2bf23a5eb6e800d9b293dfa04
Andre-gomes-dev <gomesandre749@gmail.com>
2022-01-22 12:29:10 -0300

    estruturas de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+
+var escreve = 1
+// while(escreve <= 6){
+//     console.log(`passo ${escreve}`)
+//     escreve++
+// }
+
+do{
+ 
+    console.log(`passo ${escreve}`)
+    escreve++
+   
+}  while(escreve <= 6)
+   
+
+
+    
---
+++ aula14/rep2.js
@@ -0,0 +1,4 @@
+console.log("teste")
+for(i=1; i <= 6; i++){
+console.log(`passo ${i}`)
+}
